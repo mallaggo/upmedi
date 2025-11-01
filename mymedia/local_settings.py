8177bbb7b0023e2944c34f863e0ec05dac600ac0
mallaggo <wavecanon@naver.com>
2025-11-01 15:03:44 +0900

    remove

--- mymedia/local_settings.py
+++
@@ -1,6 +0,0 @@
-SECRET_KEY = 'django-insecure-g(76jgb1%%x=rh((x-m46nhm67@l1$-20bfg9-^ql#swalf&w1'
-EMAIL_HOST_USER = 'wavecanon@naver.com'
-EMAIL_HOST_PASSWORD = 'SNMQEN583GSC'
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-# 이메일 인증 링크에 쓸 서비스 도메인
-SITE_DOMAIN = "http://www.lee95.com"
