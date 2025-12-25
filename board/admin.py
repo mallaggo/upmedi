1cc14237a68f821f940ea252179134e6ac472896
mallaggo <wavecanon@naver.com>
2025-12-25 12:08:40 +0900

    test up

--- board/admin.py
+++ board/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Com2,Com1_board,Com1_movie
+from .models import Com2,Com2_movie,Com1_board,Com1_movie
 
 # Register your models here.
 admin.site.register(Com2)
+admin.site.register(Com2_movie)
 admin.site.register(Com1_board)
 admin.site.register(Com1_movie)
